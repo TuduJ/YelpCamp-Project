2d61211690e07a8743b756c492d36968fb974238
Jitendranth Tudu <jitendratd@gmail.com>
2020-07-01 19:07:46 +0000

    comment section added

--- v4/app.js
+++ v4/app.js
@@ -3,6 +3,7 @@ var app 		= express();
 var bodyParser 	= require("body-parser");
 var mongoose 	= require("mongoose");
 var Campground	= require("./models/campground");
+var Comment		= require("./models/comment");
 var seedDB		= require("./seeds");
 
 
@@ -96,9 +97,35 @@ app.get("/campgrounds/:id", function(req, res){
 // ====================
 
 app.get("/campgrounds/:id/comments/new", function(req, res){
-	res.render("comments/new");
+// 	find campground by id
+	Campground.findById(req.params.id, function(err, campground){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("comments/new", {campground: campground});
+		}
+	});
 });
 
+
+app.post("/campgrounds/:id/comments", function(req, res){
+	Campground.findById(req.params.id, function(err, campground){
+		if(err){
+			console.log(err);
+			res.redirect("/campgrounds")
+		}else{
+			Comment.create(req.body.comment, function(err, comment){
+				if(err){
+					console.log(err);
+				}else{
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/campgrounds/" + campground._id)
+				}
+			});
+		}
+	});
+});
 
 
  
