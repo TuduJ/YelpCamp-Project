2c92756c86dd3ba408b826194ff63c2cabe41964
Jitendranth Tudu <jitendratd@gmail.com>
2020-07-08 21:13:51 +0000

    alert added

---
+++ v11/routes/campgrounds.js
@@ -0,0 +1,96 @@
+var express 	= require("express");
+var router 		= express.Router();
+
+var Campground 	= require("../models/campground");
+var middleware 	= require("../middleware");
+
+// INDEX - Show all campgrounds
+router.get("/", function(req, res){
+// 	Get all the campgrounds from DB 
+	Campground.find({}, function(err, allCampgrounds){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("campgrounds/index", {campgrounds: allCampgrounds});
+		}
+	});
+});
+
+
+// CREATE - Add new campgrounds to the database
+router.post("/", middleware.isLoggedIn, function(req, res){
+	var name 	= req.body.name;
+	var image 	= req.body.image;
+	var desc 	= req.body.description;
+	var author 	= {
+		id: req.user._id,
+		username: req.user.username
+	};
+	var newCampground = {name: name, image: image, description: desc, author: author}
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		}else{
+// 			Redirect back to Campgrounds page
+			console.log(newlyCreated);
+			res.redirect("/campgrounds");
+		}
+	})
+});
+
+
+
+// NEW - Show form to add new campground
+router.get("/new", middleware.isLoggedIn, function(req, res){
+	res.render("campgrounds/new");
+});
+
+
+// SHOW - Dhow details page of the campground
+router.get("/:id", function(req, res){
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+		if(err){
+			console.log(err);
+		}else{
+			console.log(foundCampground);
+			//render show template with that campground
+			res.render("campgrounds/show", {campground: foundCampground});
+		}
+	});
+});
+
+// Edit Campground Route
+router.get("/:id/edit", middleware.checkCampgroundOwnership, function(req, res){
+
+	Campground.findById(req.params.id, function(err, foundCampground){
+		res.render("campgrounds/edit", {campground: foundCampground});
+	});
+});
+
+
+// Update campground Route
+router.put("/:id", middleware.checkCampgroundOwnership, function(req, res){
+	Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+		if(err){
+			res.redirect("/campgrounds");
+		}
+		else{
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
+
+// Destroy campground Route
+router.delete("/:id", middleware.checkCampgroundOwnership, function(req, res){
+	Campground.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/campgrounds");
+		}else{
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+
+module.exports = router;
